d0c66f32830e7f1e2e829bea20d8cf55c9f2b3ce
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-08-28 19:57:12 +0600

    File Operation
    File Writing using fs.writeFile()

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+// Server Setup
 var http = require("http");
+var fs = require("fs");
 
 var server = http.createServer(function (req, res) {
+  // Route Handling
   if (req.url == "/") {
     res.end("This is the Home Page");
   } else if (req.url == "/about") {
     res.end("This is the About Page");
   } else if (req.url == "/contact") {
     res.end("This is the Contact Page");
+  }
+
+  //File Operation
+
+  if (req.url == "/file-write") {
+    fs.writeFile("demo.txt", "hello world", function (error) {
+      if (error) {
+        res.end("File Write Failed!");
+      } else {
+        res.end("File Write Successful!");
+      }
+    });
   }
 });
 
